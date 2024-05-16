8be4ce450253eeae056158cc18848a0f1f9e5f01
Julia Mularczyk <80833377+julia-mula@users.noreply.github.com>
2024-05-16 17:11:59 +0000

    task 5 - validation

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,11 @@
 class Ingredient < ApplicationRecord
   belongs_to :recipe
+  
+  validate :validate_number_of_ingredients_in_recipe
+
+  def validate_number_of_ingredients_in_recipe
+    if recipe.ingredients.count >= 3
+      errors.add(:base, "A recipe can have a maximum of 3 ingredients")
+    end
+  end
 end
